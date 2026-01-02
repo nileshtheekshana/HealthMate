7fff66a1ea6ebdc4a00bf0c29fe0787e75106394
nilesh theekshana <65758863+nileshtheekshana@users.noreply.github.com>
2026-01-02 21:22:56 +0530

    Making the template page

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthMate.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
